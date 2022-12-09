10d7030de190a7f910b8cbdec3f8b7e42077f316
jonnu <jon@ellis-jones.org>
2022-12-09 13:35:49 +0000

    wip

--- src/main/java/com/github/jonnu/advent/runner/AdventRunner.java
+++ src/main/java/com/github/jonnu/advent/runner/AdventRunner.java
@@ -2,16 +2,14 @@ package com.github.jonnu.advent.runner;
 
 import com.github.jonnu.advent.inject.AdventModule;
 import com.github.jonnu.advent.puzzle.Puzzle;
-import com.github.jonnu.advent.puzzle.y2022.Puzzle15;
+import com.github.jonnu.advent.puzzle.y2022.*;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 
 public class AdventRunner {
     public static void main(final String[] args) {
         Injector injector = Guice.createInjector(new AdventModule());
-        Puzzle puzzle = injector.getInstance(Puzzle15.class);
-        long start = System.currentTimeMillis();
+        Puzzle puzzle = injector.getInstance(Puzzle9.class);
         puzzle.solve();
-        System.out.printf("%n[Time taken: %dms]%n", System.currentTimeMillis() - start);
     }
 }
