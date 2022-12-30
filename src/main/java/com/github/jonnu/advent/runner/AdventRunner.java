66f4a0d3fa7883b0710da4a429919b34f4ab1309
jonnu <jontce@gmail.com>
2022-12-30 13:08:52 +0000

    Puzzle 7 solution (from 2021).

--- src/main/java/com/github/jonnu/advent/runner/AdventRunner.java
+++ src/main/java/com/github/jonnu/advent/runner/AdventRunner.java
@@ -9,7 +9,7 @@ import com.google.inject.Injector;
 public class AdventRunner {
     public static void main(final String[] args) {
         Injector injector = Guice.createInjector(new AdventModule());
-        Puzzle puzzle = injector.getInstance(Puzzle6.class);
+        Puzzle puzzle = injector.getInstance(Puzzle7.class);
         puzzle.solve();
     }
 }
