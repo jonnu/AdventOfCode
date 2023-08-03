770340306f66518d4ecd005b302119bc9bab9fed
jonnu <jon@ellis-jones.org>
2023-08-03 09:33:28 +0100

    Puzzle 16 solution (from 2021).

--- src/main/java/com/github/jonnu/advent/puzzle/y2021/Puzzle16.java
+++ src/main/java/com/github/jonnu/advent/puzzle/y2021/Puzzle16.java
@@ -1,20 +1,19 @@
 package com.github.jonnu.advent.puzzle.y2021;
 
+import com.github.jonnu.advent.common.ResourceReader;
+import com.github.jonnu.advent.puzzle.Puzzle;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.SneakyThrows;
+import lombok.Value;
+
+import javax.inject.Inject;
 import java.io.BufferedReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
-import javax.inject.Inject;
-
-import com.github.jonnu.advent.common.ResourceReader;
-import com.github.jonnu.advent.puzzle.Puzzle;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.SneakyThrows;
-import lombok.Value;
 
 @AllArgsConstructor(onConstructor = @__(@Inject))
 public class Puzzle16 implements Puzzle {
@@ -26,94 +25,81 @@ public class Puzzle16 implements Puzzle {
     public void solve() {
         try (BufferedReader reader = resourceReader.read("y2021/puzzle16.txt")) {
 
-            String binary = "";
-            String line = reader.readLine();
-            while (line != null) {
-                binary = hex2bin(line);
-                line = reader.readLine();
-            }
+            final String binary = hex2bin(reader.readLine());
+            final Packet packet = parse(binary);
 
-            Packet packet = parse(binary);
-            System.out.println("VersionSum: " + versionSum(packet));
+            System.out.println("sum of versions: " + versionSum(packet));
+            System.out.println("outermost value: " + packet.getValue());
         }
     }
 
-
     private static Packet parse(final String binary) {
+
         int version = parseInt(binary, 0, 3);
-        int typeId = parseInt(binary, 3, 3);
+        final Operator operator = Operator.resolve(parseInt(binary, 3, 3));
 
-        switch (typeId) {
-            case 4:
-                Literal literal = literal(binary.substring(6));
-                //System.out.println(literal);
-                return Packet.builder()
-                        .binary(binary)
-                        .version(version)
-                        .typeId(typeId)
-                        .value(literal.getValue())
-                        .length(6 + literal.getLength())
-                        .subPackets(Collections.EMPTY_LIST)
-                        .build();
-                //break;
-            default:
-                int lengthTypeId = parseInt(binary, 6, 1);
-                if (lengthTypeId == 0) {
-
-                    int bits = parseInt(binary, 7, 15);
-                    int read = 0;
-
-                    List<Packet> children = new ArrayList<>();
-                    while (read < bits) {
-                        //System.out.println("Read: " + read + ", Bits: " + bits + "(" + binary.substring(22 + read, 22 + bits) + ")");
-                        Packet child = parse(binary.substring(22 + read));
-                        children.add(child);
-                        //System.out.println("Child: " + child);
-                        read += child.getLength();
-                    }
-
-                    //System.out.println("Return from 0");
-                    return Packet.builder()
-                            .binary(binary)
-                            .version(version)
-                            .typeId(typeId)
-                            //.length(binary.length())
-                            .length(22 + read)
-                            .subPackets(children)
-                            .build();
-
-                } else {
-                    int packets = parseInt(binary, 7, 11);
-                    int process = 0;
-                    int read = 0;
-
-                    List<Packet> children = new ArrayList<>();
-                    while (process < packets) {
-                        Packet child = parse(binary.substring(18 + read));
-                        children.add(child);
-                        //System.out.println("Child: " + child);
-                        read += child.getLength();
-                        process++;
-                    }
-
-                    //System.out.println("Return from 1");
-                    return Packet.builder()
-                            .binary(binary)
-                            .version(version)
-                            .typeId(typeId)
-                            .length(18 + read)
-                            //.length(binary.length())
-                            .subPackets(children)
-                            .build();
-                }
-                //break;
+        if (operator.equals(Operator.LITERAL)) {
+            Literal literal = literal(binary.substring(6));
+            return Packet.builder()
+                    .binary(binary)
+                    .version(version)
+                    .typeId(operator.value())
+                    .value(literal.getValue())
+                    .length(6 + literal.getLength())
+                    .subPackets(Collections.emptyList())
+                    .build();
         }
 
+        int read = 0;
+        int opType = parseInt(binary, 6, 1);
+        int offset = opType == 0 ? 22 : 18;
+        List<Packet> children = new ArrayList<>();
 
+        if (opType == 0) {
+
+            int bits = parseInt(binary, 7, 15);
+
+            while (read < bits) {
+                Packet child = parse(binary.substring(offset + read));
+                children.add(child);
+                read += child.getLength();
+            }
+
+        } else {
+
+            int packetsRequired = parseInt(binary, 7, 11);
+            int packetsProcessed = 0;
+
+            while (packetsProcessed < packetsRequired) {
+                Packet child = parse(binary.substring(offset + read));
+                children.add(child);
+                read += child.getLength();
+                packetsProcessed++;
+            }
+        }
+
+        long value = 0;
+        switch (operator) {
+            case SUM -> value = children.stream().mapToLong(Packet::getValue).sum();
+            case PRODUCT -> value = children.stream().mapToLong(Packet::getValue).reduce(1, (a, b) -> a * b);
+            case MIN -> value = children.stream().mapToLong(Packet::getValue).summaryStatistics().getMin();
+            case MAX -> value = children.stream().mapToLong(Packet::getValue).summaryStatistics().getMax();
+            case GREATER_THAN -> value = children.get(0).getValue() > children.get(1).getValue() ? 1 : 0;
+            case LESS_THAN -> value = children.get(0).getValue() < children.get(1).getValue() ? 1 : 0;
+            case EQUAL_TO -> value = children.get(0).getValue() == children.get(1).getValue() ? 1 : 0;
+        }
+
+        return Packet.builder()
+                .binary(binary)
+                .version(version)
+                .value(value)
+                .typeId(operator.value())
+                .length(offset + read)
+                .subPackets(children)
+                .build();
     }
 
     private static int parseInt(final String binary, final int start, final int length) {
-        //System.out.println(binary + " (" + start + " - " + (start+length) + ") = " + binary.substring(start, start + length) + " = " + Integer.parseInt(binary.substring(start, start + length), 2));
         return Integer.parseInt(binary.substring(start, start + length), 2);
     }
 
@@ -129,18 +115,14 @@ public class Puzzle16 implements Puzzle {
 
         int pointer = 0;
         boolean isLastWord = false;
-        StringBuilder builder = new StringBuilder();
+        final StringBuilder builder = new StringBuilder();
 
-        //101111111000101
-        // 0111 1110 0101
         while (!isLastWord) {
             isLastWord = binary.charAt(pointer) == '0';
-            builder.append(binary.substring(pointer + 1, pointer + 5));
-            //System.out.printf("L: %5s, P: %02d, %s%n", isLastWord, pointer, builder);
+            builder.append(binary, pointer + 1, pointer + 5);
             pointer += 5;
         }
 
-        //System.out.println("B: " + binary + " (" + binary.length() + "); P: " + pointer);
         return new Literal(Long.parseLong(builder.toString(), 2), pointer);
     }
 
@@ -150,18 +132,17 @@ public class Puzzle16 implements Puzzle {
 
     private static int versionSum(final Packet packet, int sum) {
 
-        System.out.println(packet);
         sum += packet.getVersion();
         for (Packet cp : packet.getSubPackets()) {
             sum = versionSum(cp, sum);
         }
+
         return sum;
     }
 
-
     @AllArgsConstructor
     @Builder
-    @Data
+    @Value
     public static class Packet {
         String binary;
         int version;
@@ -176,6 +157,28 @@ public class Puzzle16 implements Puzzle {
     public static class Literal {
         long value;
         int length;
+    }
+
+    private enum Operator {
+        SUM,
+        PRODUCT,
+        MIN,
+        MAX,
+        LITERAL,
+        GREATER_THAN,
+        LESS_THAN,
+        EQUAL_TO;
+
+        public int value() {
+            return ordinal();
+        }
+
+        public static Operator resolve(final int ordinal) {
+            return Arrays.stream(Operator.values())
+                    .skip(ordinal)
+                    .findFirst()
+                    .orElse(null);
+        }
     }
 }
 
