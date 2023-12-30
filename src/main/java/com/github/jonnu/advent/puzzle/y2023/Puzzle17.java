9854668fd3139644703f15cc6ad5b2164bda75fa
jonnu <jon@ellis-jones.org>
2023-12-30 12:51:14 +0000

    progress on 17 but costs is wrong.

--- src/main/java/com/github/jonnu/advent/puzzle/y2023/Puzzle17.java
+++ src/main/java/com/github/jonnu/advent/puzzle/y2023/Puzzle17.java
@@ -1,10 +1,12 @@
 package com.github.jonnu.advent.puzzle.y2023;
 
 import java.io.BufferedReader;
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.IntSummaryStatistics;
+import java.util.List;
 import java.util.Map;
 import java.util.PriorityQueue;
 import java.util.Queue;
@@ -66,30 +68,33 @@ public class Puzzle17 implements Puzzle {
 
     private int solve(final Point origin, final Point destination) {
 
-        System.out.println(blocks);
-
         int heatLoss = 0;
         Queue<Path> queue = new PriorityQueue<>(Comparator.comparing(Path::getHeatLoss));
         final Map<Point, Integer> costs = new HashMap<>();
+        final Map<Point, Path> costs2 = new HashMap<>();
         //Set<Point> visited = new HashSet<>();
 
-        RollingStack<Direction> lookBack = new RollingStack<>(3);
-        lookBack.push(Direction.EAST);
+        //RollingStack<Direction> lookBack = new RollingStack<>(3);
+        //lookBack.push(Direction.EAST);
 
-        System.out.println("Lookback: " + lookBack);
-        Direction currentDirection = lookBack.lastElement();
+        //System.out.println("Lookback: " + lookBack);
+        //Direction currentDirection = lookBack.lastElement();
 
         Predicate<Point> isInBounds = p -> p.getX() >= 0 && p.getX() <= blocks.keySet().stream().mapToInt(Point::getX).max().orElse(0)
                 && p.getY() >= 0 && p.getY() <= blocks.keySet().stream().mapToInt(Point::getY).max().orElse(0);
 
-        queue.add(new Path(0, origin, new RollingStack<>(3), new HashSet<>()));
+        PriorityQueue<Path> p = new PriorityQueue<>(Comparator.comparing(Path::getHeatLoss));
+
+        queue.add(new Path(0, origin, new RollingStack<>(3), new ArrayList<>(List.of(origin))));
         while (!queue.isEmpty()) {
 
             Path currentPath = queue.poll();
             Point currentPosition = currentPath.getCurrentPosition();
 
             if (currentPosition.equals(destination)) {
-                break;
+                currentPath.getVisited().add(currentPosition);
+                p.add(currentPath);
+                continue;
             }
 
             //int blockHeatLoss = blocks.get(currentPosition);
@@ -98,35 +103,47 @@ public class Puzzle17 implements Puzzle {
             Set<Direction> possibleDirections = Direction.cardinal()
                     .stream()
                     // rule: cannot go back on itself
-                    .filter(d -> !d.equals(currentDirection.opposite()))
+                    .filter(d -> currentPath.getLookbackDirections().isEmpty() || !d.equals(currentPath.getLookbackDirections().peek().opposite()))
                     // rule: cannot go the same way three times.
-                    .filter(d -> !(lookBack.isFull() && lookBack.isHomogeneous() && lookBack.contains(d)))
+                    .filter(d -> !(currentPath.getLookbackDirections().isFull() && currentPath.getLookbackDirections().isHomogeneous() && currentPath.getLookbackDirections().contains(d)))
                     // rule: cannot go out of bounds.
-                    .filter(d -> isInBounds.test(currentPosition.move(d)))
+                    .filter(d -> isInBounds.test(currentPath.getCurrentPosition().move(d)))
                     .collect(Collectors.toSet());
 
-            //System.out.println("Possible: " + possibleDirections);
+            System.out.println("+ Path: " + currentPath);
+            System.out.println(" ? Possible: " + possibleDirections + " (Last: " + (currentPath.getLookbackDirections().isEmpty() ? "NONE" : currentPath.getLookbackDirections().peek()) + ")");
 
             possibleDirections.forEach(direction -> {
 
-                Point nextPosition = currentPosition.move(direction);
+                Point nextPosition = currentPath.getCurrentPosition().move(direction);
+
                 int loss = currentPath.getHeatLoss() + blocks.get(nextPosition);
-                if (!costs.containsKey(nextPosition) || loss < costs.get(nextPosition)) {
+                //System.out.println("Moving from " + currentPath.getCurrentPosition() + " to " + nextPosition + " (" + direction + ") = " + loss);
+                //if (!costs.containsKey(nextPosition) || loss < costs.get(nextPosition)) {
+                if (!costs2.containsKey(nextPosition) || loss <= costs2.get(nextPosition).getHeatLoss()) {
 
                     costs.put(nextPosition, loss);
 
-                    RollingStack<Direction> nextLookback = currentPath.getLookbackDirections();
-                    nextLookback.add(direction);
+                    RollingStack<Direction> nextLookback = new RollingStack<>(currentPath.getLookbackDirections());
+                    nextLookback.push(direction);
 
-                    Set<Point> nextVisited = currentPath.getVisited();
+                    List<Point> nextVisited = new ArrayList<>(currentPath.getVisited());
                     nextVisited.add(nextPosition);
 
-                    queue.add(new Path(
+                    if (destination.equals(nextPosition)) {
+                        System.out.println("XPath:" + currentPath);
+                    }
+
+                    Path x = new Path(
                             currentPath.getHeatLoss() + blocks.get(nextPosition),
                             nextPosition,
                             nextLookback,
                             nextVisited
-                    ));
+                    );
+                    System.out.println(" >> Pre Path: " + currentPath);
+                    System.out.println(" >> New Path: " + x);
+                    costs2.put(nextPosition, x);
+                    queue.add(x);
                     //frontier.add(new Puzzle15.Path(cost, point));
                     //movement.put(point, path.getPoint());
                 }
@@ -148,11 +165,17 @@ public class Puzzle17 implements Puzzle {
             //draw(blocks, currentPath.getVisited());
         }
 
-        System.out.println(costs);
-        System.out.println(destination + ": " + costs.get(destination));
+        System.out.println("QUEUE: " + p);
+        Path winner = p.poll();
+        //System.out.println(costs);
+        //System.out.println(destination + ": " + costs.get(destination));
 
-        draw(blocks, costs.keySet());
-        return heatLoss;
+        Point interest = new Point(2, 1);
+        System.out.println(interest + ":" + blocks.get(interest) + ": " + costs2.get(interest));
+
+        draw(blocks, winner.getVisited().stream().collect(Collectors.toSet()));
+        //costs2.get(destination).getVisited().forEach(System.out::println);
+        return winner.getHeatLoss();
     }
 
     private static final String ANSI_RED = "\u001B[31m";
@@ -165,7 +188,7 @@ public class Puzzle17 implements Puzzle {
         for (int y = yStats.getMin(); y <= yStats.getMax(); y++) {
             for (int x = xStats.getMin(); x <= xStats.getMax(); x++) {
                 Point p = new Point(x, y);
-                System.out.print(highlight.contains(p) ? ANSI_RED + "▒" + ANSI_RESET : grid.get(p));
+                System.out.print(highlight.contains(p) ? ANSI_RED + grid.get(p) + ANSI_RESET : grid.get(p));
             }
             System.out.printf("%n");
         }
@@ -180,7 +203,7 @@ public class Puzzle17 implements Puzzle {
         final int heatLoss;
         final Point currentPosition;
         final RollingStack<Direction> lookbackDirections;
-        @Setter Set<Point> visited;
+        @Setter List<Point> visited;
     }
 
     private static class RollingStack<T> extends Stack<T> {
@@ -189,6 +212,11 @@ public class Puzzle17 implements Puzzle {
 
         RollingStack(final int size) {
             this.size = size;
+        }
+
+        RollingStack(final RollingStack<T> source) {
+            this.size = source.size;
+            source.forEach(this::push);
         }
 
         public T push(final T item) {
