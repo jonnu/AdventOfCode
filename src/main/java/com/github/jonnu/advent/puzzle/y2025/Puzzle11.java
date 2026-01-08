457538aa2c50be00b4fe4139e833ace27487ad8a
jonnu <jon@ellis-jones.org>
2026-01-08 07:46:56 +0000

    in progress

--- src/main/java/com/github/jonnu/advent/puzzle/y2025/Puzzle11.java
+++ src/main/java/com/github/jonnu/advent/puzzle/y2025/Puzzle11.java
@@ -2,22 +2,25 @@ package com.github.jonnu.advent.puzzle.y2025;
 
 import com.github.jonnu.advent.common.ResourceReader;
 import com.github.jonnu.advent.puzzle.Puzzle;
-import com.github.jonnu.advent.puzzle.y2023.Puzzle8;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 
 import javax.inject.Inject;
 import java.io.BufferedReader;
 import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Queue;
 import java.util.Set;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 @AllArgsConstructor(onConstructor = @__(@Inject))
@@ -25,11 +28,11 @@ public class Puzzle11 implements Puzzle {
 
     private final ResourceReader resourceReader;
 
+    private static final Map<String, Node> NODEMAP = new HashMap<>();
+
     @Override
     @SneakyThrows
     public void solve() {
-
-        Map<String, Node> nodes = new HashMap<>();
         try (BufferedReader reader = resourceReader.read("y2025/puzzle11.txt")) {
             String line = reader.readLine();
             while (line != null) {
@@ -38,28 +41,79 @@ public class Puzzle11 implements Puzzle {
                 node.setConnections(Arrays.stream(parts[1].trim().split(" "))
                         .map(Node::new)
                         .collect(Collectors.toSet()));
-                nodes.put(node.getName(), node);
+                NODEMAP.put(node.getName(), node);
                 line = reader.readLine();
             }
-
-            System.out.println(nodes);
         }
-        nodes.put("out", new Node("out"));
+        NODEMAP.put("out", new Node("out"));
 
         // let's walk.
-        Queue<Node> queue = new ArrayDeque<>(Set.of(nodes.get("you")));
-        while (!queue.isEmpty()) {
-            Node node = queue.poll();
-            System.out.println("Reading: " + node);
-            nodes.get(node.getName()).getConnections()
-                    .stream()
-                    .map(x -> nodes.get(x.getName()))
-                            .forEach(queue::add);
+        Path you = new Path();
+        you.add(NODEMAP.get("you"));
 
-            //queue.addAll(nodes.get(node.getName()).getConnections());
+        Path svr = new Path();
+        svr.add(NODEMAP.get("svr"));
+
+        Set<Path> part1 = getValidPaths(you, path -> true);
+        Set<Path> part2 = getValidPaths(svr, path -> new HashSet<>(path.getNodes()).containsAll(Set.of(NODEMAP.get("fft"), NODEMAP.get("dac"))));
+
+        System.out.println("[Part 1] Paths from 'you' to 'out': " + part1.size());
+        System.out.println("[Part 2] Paths from 'srv' to 'out' (via fft and dac): " + part2.size());
+    }
+
+    //gonna need backtracking.
+    private Set<Path> getValidPaths(final Path start, Predicate<Path> predicate) {
+        final Set<Path> result = new HashSet<>();
+        final Set<Path> seen = new HashSet<>();
+        final Queue<Path> queue = new ArrayDeque<>(Set.of(start));
+        while (!queue.isEmpty()) {
+            Path path = queue.poll();
+            //System.out.println("Eval: " + path);
+            if (seen.contains(path)) {
+                continue;
+            }
+            if (path.tail().getName().equals("out") && predicate.test(path)) {
+                result.add(path);
+                continue;
+            }
+            queue.addAll(path.possibilities());
+            seen.add(path);
+        }
+        return result;
+    }
+
+    @Getter
+    private static class Path {
+
+        List<Node> nodes = new ArrayList<>();
+
+        public void add(final Node node) {
+            nodes.add(NODEMAP.get(node.getName()));
         }
 
-        //System.out.println("[Part 1] " + you);
+        public Node tail() {
+            return nodes.getLast();
+        }
+
+        // from the given path, return a list of outcomes.
+        public Set<Path> possibilities() {
+            return tail().getConnections()
+                    .stream()
+                    .map(this::copyWith)
+                    .collect(Collectors.toSet());
+        }
+
+        public Path copyWith(final Node node) {
+            Path path = new Path();
+            getNodes().forEach(path::add);
+            path.add(node);
+            return path;
+        }
+
+        @Override
+        public String toString() {
+            return nodes.stream().map(Node::getName).collect(Collectors.joining("->"));
+        }
     }
 
     @Data
